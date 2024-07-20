ab687b3cdedf8f27fa664c5ce6566089df1b78ee
Vidhi1808 <vidhisingla99@gmail.com>
2024-07-20 22:26:26 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// var typed= new Typed(".text",{
+//   strings:["Student","Data Scientist","Web Developer"],
+//   typeSpeed:100,
+//   backSpeed:100,
+//   backDelay:1000,
+//   loop:true
+// });
+
+var typed = new Typed(".text", {
+  strings: ["Student", "Data Scientist", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+  
+});
